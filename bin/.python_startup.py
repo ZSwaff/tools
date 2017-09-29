c9d65077aacdeaf7a6f6a0e6ef37b5d8808e528f
Zack Swafford <zdfs1121@gmail.com>
2017-09-29 14:41:18 -0700

    Updated python startup

--- bin/.python_startup.py
+++ bin/.python_startup.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import os
+import random
 
 import numpy as np
 
@@ -9,3 +10,14 @@ d = {1: 2, 2: 3, 3: 4}
 t = (1, 2, 3)
 
 q = exit
+
+def rand_sn(prefix='', n=1):
+	def to_let(i):
+		if i < 10: 
+			return str(i)
+		return chr(i - 10 + ord('A'))
+	res = []
+	for i in range(n):
+		lst = [to_let(random.randint(0, 35)) for j in range(10)]
+		res.append('{}{}{}{}{}-{}{}{}{}-{}{}'.format(prefix, *lst))
+	return res
