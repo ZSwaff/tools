15a364e7fedba35472f6e07e22fb308587d409f6
Zack Swafford <zdfs1121@gmail.com>
2017-10-03 12:40:25 -0700

    Updated git clone

--- bin/.python_startup.py
+++ bin/.python_startup.py
@@ -11,7 +11,7 @@ t = (1, 2, 3)
 
 q = exit
 
-def rand_sn(prefix='', n=1):
+def rand_sn(n=1, prefix='', suffix='', sn_format='{}{}{}{}-{}{}{}{}-{}{}{}'):
 	def to_let(i):
 		if i < 10: 
 			return str(i)
@@ -19,5 +19,10 @@ def rand_sn(prefix='', n=1):
 	res = []
 	for i in range(n):
 		lst = [to_let(random.randint(0, 35)) for j in range(10)]
-		res.append('{}{}{}{}{}-{}{}{}{}-{}{}'.format(prefix, *lst))
+		res.append(prefix + sn_format.format(*lst) + suffix)
 	return res
+
+# Trays
+# print '\n'.join(rand_sn(30, '', '', 'Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}'))
+# Towers
+# print '\n'.join(rand_sn(12, '', '', 'Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}'))
