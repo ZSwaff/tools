2e025e3fbbd3bacf49bb56d81a6ccff5f62ea270
Zack Swafford <zdfs1121@gmail.com>
2017-11-07 18:20:36 -0800

    Added tray creation functions to python initer

--- bin/.python_startup.py
+++ bin/.python_startup.py
@@ -22,6 +22,12 @@ def rand_sn(n=1, prefix='', suffix='', sn_format='{}{}{}{}-{}{}{}{}-{}{}{}'):
 		res.append(prefix + sn_format.format(*lst) + suffix)
 	return res
 
+def rand_towers(n=1):
+	return rand_sn(n, '', '', 'Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}')
+
+def rand_trays(n=1):
+	return rand_sn(n, '', '', 'Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}')
+
 def tower_loc(room, row, tower, site='SSF1'):
 	return 'T:{}-{}-{}-{}'.format(site, room, row, tower)
 
@@ -30,8 +36,10 @@ def formatted_tower_locs(room, rows, towers, site='SSF1'):
 	t_locs = [tower_loc(room, row, tower, site) for row, tower in locs]
 	return ['{0}, {1}, {0}'.format(e, '-'.join(e.split('-')[-2:])) for e in t_locs]
 
+def tray_loc(room, cart, level, spot, site='SSF1'):
+	return 'Y:{}-{}-{}-{}-{}'.format(site, room, cart, level, spot)
 
-# Trays
-# print '\n'.join(rand_sn(30, '', '', 'Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}'))
-# Towers
-# print '\n'.join(rand_sn(12, '', '', 'Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}'))
+def formatted_tray_locs(room, carts, levels, spots, site='SSF1'):
+	locs = [(str(c), chr(l), str(s)) for c in carts for l in range(ord('A'), ord('A') + levels) for s in range(1, spots + 1)]
+	t_locs = [tray_loc(room, cart, level, spot, site) for cart, level, spot in locs]
+	return ['{0}, {1}, {0}'.format(e, '-'.join(e.split('-')[-3:])) for e in t_locs]
