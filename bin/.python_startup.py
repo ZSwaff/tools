c38315b9f80c1d3fecc30d3a88285ad3828e9d4a
Zack Swafford <zdfs1121@gmail.com>
2018-09-11 12:15:33 -0700

    Various improvements

--- bin/.python_startup.py
+++ bin/.python_startup.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Common Python functions."""
+
 import sys
 import os
+import math
 import time
 import datetime
 import random
@@ -8,49 +13,21 @@ import re
 import subprocess
 
 import numpy as np
+import pandas as pd
 
 import plentyservice
+import plenty_data_frames
+
+from common import *
 
 
 false = False
 true = True
 null = None
 
-l = [1, 2, 3, 4, 5]
+s = 'abc'
+l = [1, 2, 3]
 d = {1: 2, 2: 3, 3: 4}
 t = (1, 2, 3)
 
 q = exit
-
-def rand_sn(n=1, prefix='', suffix='', sn_format='{}{}{}{}-{}{}{}{}-{}{}{}'):
-	def to_let(i):
-		if i < 10: 
-			return str(i)
-		return chr(i - 10 + ord('A'))
-	res = []
-	for i in range(n):
-		lst = [to_let(random.randint(0, 35)) for j in range(10)]
-		res.append(prefix + sn_format.format(*lst) + suffix)
-	return res
-
-def rand_towers(n=1):
-	return rand_sn(n, '', '', 'Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tower:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}')
-
-def rand_trays(n=1):
-	return rand_sn(n, '', '', 'Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}, {0}{1}{2}{3}, Tray:0_0_1:{0}{1}{2}{3}-{4}{5}{6}{7}-{8}{9}')
-
-def tower_loc(room, row, tower, site='SSF1'):
-	return 'T:{}-{}-{}-{}'.format(site, room, row, tower)
-
-def formatted_tower_locs(room, rows, towers, site='SSF1'):
-	locs = [(chr(r), str(n)) for r in range(ord('A'), ord('A') + rows) for n in range(1, towers + 1)]
-	t_locs = [tower_loc(room, row, tower, site) for row, tower in locs]
-	return ['{0}, {1}, {0}'.format(e, '-'.join(e.split('-')[-2:])) for e in t_locs]
-
-def tray_loc(room, cart, level, spot, site='SSF1'):
-	return 'Y:{}-{}-{}-{}-{}'.format(site, room, cart, level, spot)
-
-def formatted_tray_locs(room, carts, levels, spots, site='SSF1'):
-	locs = [(str(c), chr(l), str(s)) for c in carts for l in range(ord('A'), ord('A') + levels) for s in range(1, spots + 1)]
-	t_locs = [tray_loc(room, cart, level, spot, site) for cart, level, spot in locs]
-	return ['{0}, {1}, {0}'.format(e, '-'.join(e.split('-')[-3:])) for e in t_locs]
