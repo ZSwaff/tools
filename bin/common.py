10471bd8cddea4783c8f5f3ea466a8d3f97c4d5f
Zack Swafford <zdfs1121@gmail.com>
2020-05-15 15:35:00 -0700

    Restructure RPCL into own class

--- bin/common.py
+++ bin/common.py
@@ -3,14 +3,9 @@
 """Common Python functions and classes."""
 
 
-from __future__ import print_function
-import builtins
 import os
-import sys
-import traceback
 import subprocess
 from datetime import datetime
-from functools import wraps
 
 from termcolor import colored
 
@@ -123,14 +118,38 @@ def load_j(fname):
 
 
 def color_info(s):
+    """Color an object as info.
+
+    Args:
+        s (str): The object to color.
+
+    Returns:
+        (dict): The colored object.
+    """
     return colored(str(s), 'blue')
 
 
 def color_err(s):
+    """Color an object as error.
+
+    Args:
+        s (str): The object to color.
+
+    Returns:
+        (dict): The colored object.
+    """
     return colored(str(s), 'red')
 
 
 def color_succ(s):
+    """Color an object as success.
+
+    Args:
+        s (str): The object to color.
+
+    Returns:
+        (dict): The colored object.
+    """
     return colored(str(s), 'green')
 
 
@@ -157,96 +176,3 @@ def run_command(cmd, path=None, verbose=False):
     if verbose:
         print(color_succ(result))
     return result
-
-
-def rpcl_safe(fn):
-    """Annotation for RPCL methods to ensure safe finish."""
-    @wraps(fn)
-    def wrapper():
-        try:
-            fn()
-        except Exception as ex:
-            if isinstance(ex, SystemExit):
-                raise
-            print(traceback.format_exc())
-            print(f'{RPCL_CD_DELIM}.')
-
-    return wrapper
-
-
-class RpclIoManager:
-    """A class to manage the Run Print Cd Ls client relationship."""
-
-    def __init__(self, parser):
-        """Prepares the parser.
-
-        Args:
-            parser (argparser.ArgumentParser): The parser to build on.
-        """
-        self.__parser = parser
-        self.__parser.add_argument('--rpcl-input-args', required=True)
-        self.__old_parse_args = self.__parser.parse_args
-        self.__parser.parse_args = self.parse_args
-
-        self.__input_args = None
-        self.__input_index = 0
-
-        self.__old_print = builtins.print
-        builtins.print = self.print
-        self.__old_input = builtins.input
-        builtins.input = self.input
-        builtins.exit = self.exit
-
-    def __raise_if_unparsed(self):
-        if self.__input_args is None:
-            raise RuntimeError('parse_args has not been called')
-
-    def parse_args(self):
-        """Parses the args from the command line.
-
-        Returns:
-            (argparser.Namespace): The argparser-parsed args.
-        """
-        args = self.__old_parse_args()
-        input_args_str = args.rpcl_input_args[1:-1]
-        if len(input_args_str) != 0:
-            self.__input_args = input_args_str[1:-1].split('""')
-        else:
-            self.__input_args = []
-        delattr(args, 'rpcl_input_args')
-        return args
-
-    def input(self):
-        """Gets the appropriate input from the user, one way or another.
-
-        Returns:
-            (str): The user input.
-        """
-        self.__raise_if_unparsed()
-        if self.__input_index == len(self.__input_args):
-            self.exit()
-        self.__input_index += 1
-        return self.__input_args[self.__input_index - 1]
-
-    def print(self, *args, **kwargs):
-        """Print a message appropriately (only the first time)."""
-        self.__raise_if_unparsed()
-        if self.__input_index == len(self.__input_args):
-            self.__old_print(*args, **kwargs)
-
-    def exit(self, *args, **kwargs):
-        """Print the return message and exits."""
-        self.__raise_if_unparsed()
-        self.__old_print(f'{RPCL_CD_DELIM}', end='')
-        if len(args) == 0:
-            args = ('.',)
-        self.__old_print(*args, **kwargs)
-        self.close()
-        sys.exit(0)
-
-    def close(self):
-        """Cleans up the manager."""
-        self.__raise_if_unparsed()
-        builtins.print = self.__old_print
-        builtins.input = self.__old_input
-        delattr(builtins, 'exit')
